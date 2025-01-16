b41e01ac45d66827578fcfe446cfe3dc56ae505a
ankitnathtiwari6 <ankitnathtiwari6@gmail.com>
2025-01-16 22:01:18 +0530

    github pages 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar.tsx";
 import { Home } from "./components/home-page/HomePage.tsx";
 import AboutPage from "./components/about/about.tsx";
